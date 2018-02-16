67f1d226285de020b0b69a248d52aa4f43f05ff3
Lia5 <Liaforweb@gmail.com>
2018-02-16 11:20:31 +0200

    ьутг

--- app/js/common.js
+++ app/js/common.js
@@ -1,3 +1,9 @@
 $(function(){
-    
+    $(".menu-toggle").on('click', function() {
+        $(this).toggleClass("on");
+        $('.main-menu').toggleClass("on");
+        $("nav ul").toggleClass('hidden');
+      });
+  
+  
 })
