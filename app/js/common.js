5b4b9a9849222281ce5a6c88794fa9a610359f05
Lia5 <Liaforweb@gmail.com>
2018-02-18 05:35:40 +0200

    anim main js

--- app/js/common.js
+++ app/js/common.js
@@ -1,9 +1,20 @@
 $(function(){
-    $(".menu-toggle").on('click', function() {
-        $(this).toggleClass("on");
-        $('.main-menu').toggleClass("on");
-        $(".main-header__mobile nav ul").toggleClass('hidden');
-      });
-  
+  $(".menu-toggle").on('click', function() {
+    $(this).toggleClass("on");
+    $('.main-menu').toggleClass("on");
+    $(".main-header__mobile nav ul").toggleClass('hidden');
+  });
+
+  $("#catalog-href").on('click', function() {
+    $(this).toggleClass("on");
+    $('.block2').addClass("block2--move-to-left");
+    $('.block2').addClass("slide-left");
+    $('.catalog--anim--hidden').addClass(" catalog--anim--visible");
+    $('.catalog-page--anim--hidden').addClass("catalog-page--anim");
+    $('.catalog-page--anim--hidden').addClass("slide-down");
+    $('.block2 div').addClass("anim--hidden");
+    $('.left-side__desctop').addClass("anim--hidden");
+  });
+
   
 })
