2932a069b6e66c19e16369e2cb435e04acaf4ec9
Lia5 <Liaforweb@gmail.com>
2018-03-20 16:38:54 +0200

    mob cat scroll

--- app/js/common.js
+++ app/js/common.js
@@ -13,55 +13,6 @@ $(function(){
   //  $('.catalog-page--anim--hidden').toggleClass("hidden");
   });
 
-  // $('.catalog-href').on('click', function() {
-  //   $(this).toggleClass('on');
-  //   $('.block2').addClass('block2--move-to-left');
-  //   $('.block2').removeClass('wow');
-  //   $('.block2').removeClass('fadeInRight');
-
-  //   $('.block2').addClass('slide-left');
-  //   $('.nav-right').addClass('wow');
-  //   $('.nav-right').addClass('fadeInRight');
-  //   // $('.nav-right').setAttribute('data-wow-delay', '1s');
-  //   $('.catalog-title--anim').removeClass('anim--hidden');
-  //   $('.catalog-page--anim--hidden').addClass('catalog-page--anim');
-  //   $('.catalog-page--anim--hidden').addClass('wow');
-  //   $('.catalog-page--anim--hidden').addClass('fadeInDown');
-  //   $('.catalog-page--anim--hidden').addClass('slide-down');
-  //   $('.main-header__mobile .fa-instagram').addClass('fa-instagram--anim');
-  //   $('.nav-right .catalog').addClass('anim--hidden');
-  //   $('.logo').addClass('anim--hidden');
-  //   $('.main-header__desctop').addClass('anim--hidden');
-  //   $('.main-menu__contacts').addClass('main-menu__contacts--anim');
-  //   $('.main-wrapper').addClass('main-wrapper--anim');
-  //   $('.nav-right').addClass('nav-right--anim');
-  //   $('.nav-right a').addClass('nav-right--color');
-  //   $('.nav-right .fa-instagram').addClass('nav-right--color');
-  //   $('.main-menu__item').removeClass('anim--hidden');
-  //   $('.catalog__tomain').removeClass('anim--hidden');
-  //   $('.main-page').addClass('main-page--anim');
-  //   $('.main-menu__phone').addClass('main-menu__phone--anim');
-  //   $('.nav-right .overlay').toggleClass('overlay--anim');
-  //   $('.nav-right-wrap').addClass('nav-right-wrap--anim');
-  // });
-
-  // $(".mt__item").on('click', function() {
-  //   $('.block2').addClass("block2--move-to-left-mt");
-  //   $('.block2').addClass("slide-left");
-  //   $('.mt-page--anim--hidden ').addClass("mt-page--anim--visible");
-  //   $('.mt__title--exampl').addClass("anim--hidden");
-  //   $('.mt__subtitle').addClass("mt__subtitle--anim");
-  //   $('.block2__container .social-icon').addClass("social-icon--anim");
-  //   // $('.logo img').addClass("mk-logo--anim");
-  //   $('.block2--move-to-left-mt').addClass("block2--move-to-left-mt--fix");
-  //   $('.mt-descr').addClass("anim--hidden");
-  //   $('.mt__item rotate').addClass("anim--hidden");
-  //   $('.back-to').removeClass("anim--hidden");
-  //   $('.back-to-mt').addClass("back-to-mt--anim");
-  //   $('.hight-eco').addClass("anim--hidden");
-  //   $('.main-wrapper__mt').addClass("main-wrapper__mt--anim");
-  // });
-  
   $(".examples-panel-item--block").on('click', function() {
     $('.examples__img').addClass("examples__img-blocks");
   });
@@ -69,40 +20,6 @@ $(function(){
     $('.examples__img').removeClass("examples__img-blocks");
   });
   
-	// //скрыть весь контент
-	// $('.tab_content').hide();
-	// //Показать контент первой вкладки
-	// $('.tab_content:first').show();
-	// //Активировать первую вкладку
-	// $('.tabs li:first').addClass('active');
-	// //Событие по клику
-	// $('.tabs li').click(function(event) {
-	// 	//Удалить "active" класс
-	// 	$('.tabs li').removeClass('active');
-	// 	//Добавить "active" для выбранной вкладки
-	// 	$(this).addClass('active');
-	// 	//Скрыть контент вкладки
-	// 	$('.tab_content').hide();
-
-	// 	//Найти значение атрибута ссылки, чтобы 
-	// 	//определить активный таб контент
-	// 	var selectTab = $(this).find('a').attr("href");
-	// 	//Исчезновение активного контента
-	// 	$(selectTab).fadeIn();
-  // });
-  
-  // /*tabs inside*/
-  // (function($) {
-  //   $(function() {
-     
-  //     $('.tabs__caption-exm').on('click', 'li:not(.active-exm)', function() {
-  //       $(this)
-  //         .addClass('active-exm').siblings().removeClass('active-exm')
-  //         .closest('div.tabs-exm').find('div.tabs__content-exm').removeClass('active-exm').eq($(this).index()).addClass('active-exm');
-  //     });
-     
-  //   });
-  //   })(jQuery);
 //popups
     $(function() {
       //----- OPEN
